cee39930d4263f38da981f1785912875ea18022c
Isaac Kim <myong328@gmail.com>
2017-09-18 10:49:24 -0700

    final product - custom view is working - UI is in good shape - all lines are align

--- CustomViewExcercise/ViewController.swift
+++ CustomViewExcercise/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var photoImageView: UIImageView!
     
     @IBOutlet weak var ratingControl: RatingControl!
-    
+       
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -79,11 +79,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-        
-        
-    }
     
 
 }
